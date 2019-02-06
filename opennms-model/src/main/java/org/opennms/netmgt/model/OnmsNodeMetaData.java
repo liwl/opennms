78c74fe0993ab63d3acb4558164d1be0013bf2a0
Dustin Frisch <fooker@lab.sh>
2019-02-06 14:37:32 +0100

    Make meta data entity embedded

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsNodeMetaData.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsNodeMetaData.java
@@ -33,6 +33,8 @@ import java.util.Objects;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -43,38 +45,23 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 @XmlRootElement(name="meta-data")
-@Entity
-@Table(name="node_metadata")
+@Embeddable
 public class OnmsNodeMetaData implements Serializable {
 
     private static final long serialVersionUID = 3529745790145204662L;
 
-    private OnmsNode node;
     private String context;
     private String key;
     private String value;
 
     public OnmsNodeMetaData() { }
 
-    public OnmsNodeMetaData(OnmsNode node, String context, String key, String value) {
-        this.node = Objects.requireNonNull(node);
+    public OnmsNodeMetaData(String context, String key, String value) {
         this.context = Objects.requireNonNull(context);
         this.key = Objects.requireNonNull(key);
         this.value = Objects.requireNonNull(value);
     }
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "nodeid", nullable = false)
-    @XmlJavaTypeAdapter(NodeIdAdapter.class)
-    public OnmsNode getNode(){
-        return node;
-    }
-
-    public void setNode(OnmsNode node) {
-        this.node = node;
-    }
-
-    @Id
     @Column(name="context", nullable = false)
     public String getContext(){
         return context;
@@ -84,7 +71,6 @@ public class OnmsNodeMetaData implements Serializable {
         this.context = context;
     }
 
-    @Id
     @Column(name="key", nullable = false)
     public String getKey() {
         return key;
